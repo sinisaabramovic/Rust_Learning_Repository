26fb2f1f6466430b1731530fcf85df4ad0643b82
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2019-05-04 21:12:27 +0200

    no message

---
+++ Base Rust/01 Data Types/data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/Users/sinisaabramovic/Rust/git/Base\ Rust/01\ Data\ Types/data_types/target/debug/data_types.dSYM: /Users/sinisaabramovic/Rust/git/Base\ Rust/01\ Data\ Types/data_types/src/main.rs
---
+++ Base Rust/01 Data Types/data_types/target/debug/deps/data_types-d8cdd59d2cfcbf3a.d
@@ -0,0 +1,5 @@
+/Users/sinisaabramovic/Rust/git/Base Rust/01 Data Types/data_types/target/debug/deps/data_types-d8cdd59d2cfcbf3a: src/main.rs
+
+/Users/sinisaabramovic/Rust/git/Base Rust/01 Data Types/data_types/target/debug/deps/data_types-d8cdd59d2cfcbf3a.d: src/main.rs
+
+src/main.rs:
