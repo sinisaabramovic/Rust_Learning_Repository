69c5fce6f124097e55df4f4ed374dff76987788a
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2019-05-04 22:46:53 +0200

    no message

--- Base Rust/01 Data Types/data_types/src/main.rs
+++ Base Rust/01 Data Types/data_types/src/main.rs
@@ -1,9 +1,18 @@
 mod primitive_data_types;
 mod operators;
 mod scope;
+mod stack_and_heap;
 
 fn main() {
-  //primitive_data_types::run();
-  //operators::run();
-  scope::run();
+    // primitive_data_types::run();
+    // operators::run();
+//   scope::run();
+
+//   println!("SHOW MY MEANING_OF_LIFE = {} OUTSIDE THE MODULE", scope::MEANING_OF_LIFE);
+//   println!("SHOW MY SOME_UNMUTE_STATIC = {} OUTSIDE THE MODULE", scope::SOME_UNMUTE_STATIC);
+//   unsafe {
+//         println!("SHOW MY SOME_STATIC = {} OUTSIDE THE MODULE", scope::SOME_STATIC);
+//   }
+
+    stack_and_heap::run();
 }
--- Base Rust/01 Data Types/data_types/src/scope.rs
+++ Base Rust/01 Data Types/data_types/src/scope.rs
@@ -1,5 +1,9 @@
 use std::mem;
 
+pub const MEANING_OF_LIFE :u8 = 42; // no fixed address
+pub static mut SOME_STATIC :u8 = 43;
+pub static SOME_UNMUTE_STATIC :u8 = 44;
+
 fn scope_and_shadowing() {
     let _some_variable = 127;
 
@@ -17,4 +21,14 @@ fn scope_and_shadowing() {
 
 pub fn run() {
     scope_and_shadowing();
+
+    // OK using unsafe is not the smartest idea
+    // but it neccesseary when we wish to use mutable static variables
+    unsafe {
+        println!("SHOW MY STATIC = {}", SOME_STATIC);
+        SOME_STATIC = 56;
+    }    
+
+    // if the static variable is non static then we can just use it as is (like a constant, yeahhh)
+    println!("SHOW MY SOME_UNMUTE_STATIC = {}", SOME_UNMUTE_STATIC);
 }
